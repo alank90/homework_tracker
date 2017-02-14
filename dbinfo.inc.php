88a2e56dfec07ecec34206460f297a5e890bf436
Alan <akillian@outlook.com>
2017-02-14 13:49:08 -0500

    Commit

--- dbinfo.inc.php
+++ dbinfo.inc.php
@@ -1,18 +1,10 @@
 <?php
    
 /* Config Connection */
-<<<<<<< HEAD
 $dsn='mysql:host=localhost;dbname=hw_assignments';
 $username = 'alank';
 $password = 'alank';
 $host = 'localhost';
 $dbname = 'hw_assignments';
-=======
-$dsn='mysql:host=localhost;dbname=homework';
-$username = 'alank';
-$password = 'alank';
-$host = 'localhost';
-$dbname = 'homework';
->>>>>>> 1c63a4ca4a7468d7faf1201d80727b3e21fb8286
 
 ?> 
