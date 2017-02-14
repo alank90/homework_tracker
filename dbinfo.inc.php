f6dbcd198589cce2c8a00b36cacdfc3354d4fdb5
Alan <akillian@outlook.com>
2017-02-14 13:44:51 -0500

    Commit

---
+++ dbinfo.inc.php
@@ -0,0 +1,10 @@
+<?php
+   
+/* Config Connection */
+$dsn='mysql:host=localhost;dbname=hw_assignments';
+$username = 'alank';
+$password = 'alank';
+$host = 'localhost';
+$dbname = 'hw_assignments';
+
+?> 
--- index.php
+++ index.php
@@ -8,6 +8,37 @@
 </head>
 
 <body>
+	<?php
+
+require ("dbinfo.inc.php"); //include login info file
+
+try
+    {
+      $conn = new PDO($dsn, $username, $password);
+    }
+catch (PDOException $e)
+    {
+    $error_message=$e->getMessage();
+    echo "<h1>Resource Unavailable. Please contact the System Administrator</h1>";
+    }
+//End of Connection
+
+
+// Retrieve Homework table info via SQL
+$sql = "SELECT * FROM hw_items";
+$query = $conn->prepare($sql);
+$query->execute();
+$hw_list = array();
+
+//  fetch() method fetches the next row from a result set. Here the variable $query stores the 
+// result set of the sql Select query.
+while($hw_item = $query->fetch())
+{
+    $hw_list[] = $hw_item;
+}
+print_r($hw_list);
+ ?>
+	 
     <h1>My Homework Assignments</h1>
     <div id="new_homework" title="Enter Homework Item">
         <form method="POST" action="new_hw.php">
@@ -29,12 +60,12 @@
     <main>
         <h2>Homework Assignment List</h2>
         <div id="hw_list">
-            <?php foreach($hw_items as $hw){ ?>
-                <h3> <?php echo $hw->title; ?>
-                  <p>Due <?php echo $hw->due_date;?></p>
-                  <p>Description:<br /><textarea id="description_<?php echo $hw->hw_id; ?>" class="description" name="description"><?php echo $hw->description; } ?></textarea>
+            <?php foreach($hw_list as $hw)  { ?>
+                <h3> <?php echo $hw -> Title; ?></h3>
+                  <p>Due <?php echo $hw->Date;?></p>
+                  <p>Description:<br /><textarea  class="description" name="description"><?php echo $hw->Description; } ?></textarea>
                   </p>
-                </h3>
+                
         </div>
     </main>
 </body>
